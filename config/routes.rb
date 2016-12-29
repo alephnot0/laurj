15dd012ee7c999b9ba2b6518370b3372e8da9612
RJ Seibert <rj@RJs-MacBook-Air.local>
2016-12-28 23:15:49 -0500

    Simple HTML Main Page'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'simple/index'
+
   get 'welcome/index'
-  root 'welcome#index'
+  root 'simple#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
